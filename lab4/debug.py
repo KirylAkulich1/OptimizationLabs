b8edcc21f6c1cb95d5e3a9416a6dc34a4d915924
Kirill Akulich <kirill.akulich@softeq.com>
2021-10-29 12:11:05 +0300

    lab 4 fixed

--- lab4/debug.py
+++ lab4/debug.py
@@ -1,5 +1,5 @@
 import networkx as nx
-graph = [[1,5],[2,3],[4,8,5],[4,5],[8],[6,8,9],[7],[8],[9],[10]]
+graph = [[1],[2,3],[4,5],[4,6],[5,6,8],[7],[8,10],[9,11],[9,],[11],[11]]
 
 G = nx.DiGraph()
 for i in range(len(graph)):
@@ -8,19 +8,28 @@ for i in range(len(graph)):
 
 nx.draw_planar(G,with_labels = True)
 
+
+[print(n) for n in G.successors(5)]
+
 def find_longest_path(graph,_from,_to):
-    checked_vertexes = set()
     vertexes_to_check = [_from,]
     F = dict()
     F[_from] = (0,[])
-    while len(vertexes_to_check) != 0:
-        vertex_to_check = vertexes_to_check.pop()
-        neighbors = graph.successors(vertex_to_check)
-        for neighbor in neighbors:
-            if neighbor in checked_vertexes:
-                continue
-            path = nx.shortest_path(graph,vertex_to_check,neighbor)
-            F[neighbor] = (len(path) + F[vertex_to_check][0] , path)
+    current_layer = {_from,}
+    while len(current_layer) != 0:
+        neighbors = list()
+        for vertex in current_layer:
+            for neighbor in graph.successors(vertex):
+                neighbors.append(neighbor)
+        next_layer_set = set(neighbors)
+        for neighbor in next_layer_set:
+            F[neighbor] = (0,[])
+            for source in current_layer:
+                if nx.has_path(graph,source,neighbor):
+                    path = nx.shortest_path(graph,source,neighbor)
+                    if F[neighbor][0] < (len(path) + F[source][0]):
+                        F[neighbor] = (len(path) - 1 + F[source][0] , path)
+        current_layer = next_layer_set
     return F
 
-print(find_longest_path(G,0,10))
+print(find_longest_path(G,0,11))
